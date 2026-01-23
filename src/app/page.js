7e6767a21d8b7db0fbc996ec67dbe2bb44116143
“iobaidu5” <i.obaidu5@@gmail.com>
2026-01-23 16:04:48 +0500

    change to February

--- src/app/page.js
+++ src/app/page.js
@@ -113,7 +113,7 @@ export default function Home() {
                 <div className="flex items-center gap-2">
                   <div className="w-2 h-2 bg-[#5df115] rounded-full animate-pulse"></div>
                   <span className="px-4 py-2 bg-[#5df115] text-[#010d01] rounded-full text-sm md:text-base font-secondary font-semibold">
-                    LAUNCH: JANUARY 2026
+                    LAUNCH: FEBRUARY 2026
                   </span>
                 </div>
               </motion.div>
